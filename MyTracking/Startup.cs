83b51f79e831c39ac3ef88b081d8aee80fbcd473
Cristian Sanchez <sancas19@gmail.com>
2016-10-13 22:38:49 -0600

    Proyecto MVC creado

---
+++ MyTracking/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyTracking.Startup))]
+namespace MyTracking
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
